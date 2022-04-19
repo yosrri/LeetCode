e57d63c0914d3dbb6efabf6a6a65f8d8aaa2e9f9
yosrri <48595913+yosrri@users.noreply.github.com>
2022-04-19 13:25:47 +0200

    Time: 727 ms (20.48%), Space: 47.3 MB (18.83%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -5,9 +5,6 @@ class Solution {
         return s;
     }
     public boolean isPalindrome(String s) {
-        if(s.isEmpty()){
-            return true;
-        }
         s = sanitizedString(s);
         int i = 0;
         int j = s.length()-1;
