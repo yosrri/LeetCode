770de898902c8d4950c1468232fcde9ad5d0d874
yosrri <48595913+yosrri@users.noreply.github.com>
2022-04-19 13:17:15 +0200

    Time: 657 ms (23.84%), Space: 43.2 MB (74.77%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String sanitizedString(String s){
+         s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        return s;
+    }
+    public boolean isPalindrome(String s) {
+        s = sanitizedString(s);
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i++)!=s.charAt(j--))
+                return false;
+        }
+        return true;
+    }
+}
