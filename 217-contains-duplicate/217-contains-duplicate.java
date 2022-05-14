51d3f53b055923941da252827f182cccc85625fc
yosrri <48595913+yosrri@users.noreply.github.com>
2022-05-14 21:41:50 +0200

    Time: 14 ms (45.63%), Space: 68.6 MB (54.36%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,17 +1,6 @@
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // boolean duplicate = false;
-        // HashMap<Integer, Integer> hm = new HashMap<>();
-        // for (int num: nums){
-        //     if(hm.containsKey(num)){
-        //         return true; 
-        //     }
-        //     else 
-        //         hm.put(num,num);
-        // }
-        // return false;
-        
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int num:nums){
             if(!hm.containsKey(num)){
